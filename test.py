a63d01df232e261a3adf1945a3c3182dfe944d19
Yunlan Tang <ytang22@berkeley.edu>
2025-11-27 19:37:13 +0800

    Update according to the website change

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from CEACStatusBot import query_status,OnnxCaptchaHandle
 
 try:
     NUMBER = os.environ["NUMBER"]
-    print(query_status(NUMBER,OnnxCaptchaHandle("captcha.onnx")))
+    PASSPORT_NUM = os.environ["PASSPORT_NUM"]
+    SURNAME = os.environ["SURNAME"]
+    print(query_status(NUMBER,PASSPORT_NUM,SURNAME,OnnxCaptchaHandle("captcha.onnx")))
 except KeyError:
     print("ENV Error")
